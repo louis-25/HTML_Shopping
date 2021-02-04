13266659b7d92cf7cb4f9ca2391a28c8516801a5
louis-25 <20151577@vision.hoseo.edu>
2021-02-04 13:24:02 +0900

    JS: filltering items

--- src/main.js
+++ src/main.js
@@ -5,8 +5,9 @@ function loadItems() {
 }
 
 function displayItems(items) {
-    const container =document.querySelector('.items');
+    const container = document.querySelector('.items');
     container.innerHTML = items.map(item => createHTMLString(item)).join('');
+    // console.log(container.innerHTML);
 }
 
 function createHTMLString(item) {
@@ -18,10 +19,45 @@ function createHTMLString(item) {
     `;
 }
 
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key; //클릭한 버튼의 key
+    const value = dataset.value; // 클릭한 버튼의 value    
+
+    if(key == null || value == null) { //key와 value값이 null이면 실행X
+        return;
+    }
+
+    // updateItems(items, key, value);
+    displayItems(items.filter(item => item[key] === value));
+}
+
+// function updateItems(items, key, value) {
+//     const html_item = document.querySelector('.item');
+//     console.log(html_item);    
+//     items.forEach(item => {        
+//         console.log("item " + item[key]);
+//         console.log("value "+ value);            
+//         if (item[key] === value) {
+//             html_item.classList.remove('invisible'); //invisible옵션 제거
+//         }else {
+//             html_item.classList.add('invisible');
+//         }
+//     })    
+// }
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items)); //로고를 클릭하면 전체 아이템 보이기
+    buttons.addEventListener('click', event => onButtonClick(event, items)); //각 버튼 클릭시 타입별로 보여주기
+}
+
 // main
 loadItems()
-    .then(items => {
-        displayItems(items);
+    .then(items => {        
         console.log(items);
+        displayItems(items);
+        setEventListeners(items);        
     })
     .catch(console.log);
